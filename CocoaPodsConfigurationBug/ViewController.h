158f272421692544f883955135bf07ef4b682c68
Marcelo Fabri <marcelofabrimf@gmail.com>
2016-06-04 16:02:52 -0300

    Initial Commit

---
+++ CocoaPodsConfigurationBug/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CocoaPodsConfigurationBug
+//
+//  Created by Marcelo Fabri on 04/06/16.
+//  Copyright © 2016 Marcelo Fabri. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CocoaPodsConfigurationBug/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CocoaPodsConfigurationBug
+//
+//  Created by Marcelo Fabri on 04/06/16.
+//  Copyright © 2016 Marcelo Fabri. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
